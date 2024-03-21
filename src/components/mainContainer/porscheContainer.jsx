8fcc7003655a4333566d3becc51738adce1a710c
KalembaUG <kalembajamil01@gmail.com>
2024-03-21 11:50:09 +0300

    fix spelling for porsche

--- src/components/mainContainer/porscheContainer.jsx
+++ src/components/mainContainer/porscheContainer.jsx
@@ -5,8 +5,8 @@ const PorscheContainer = () => {
         <div className='porsche-container'>
             <div className="porsche-info">
                 <h5>Driving at it finest</h5>
-                <h3>Discover the Persche Host Program</h3>
-                <p>Skyla and Persche are proud to introduce the Persche Host Program, a pilot program in San Francisco and Los Angeles that combines curated Persche vehicles with top Skyla hosts to give you the complete Persche experience.</p>
+                <h3>Discover the Porsche Host Program</h3>
+                <p>Skyla and Porsche are proud to introduce the Porsche Host Program, a pilot program in San Francisco and Los Angeles that combines curated Porsche vehicles with top Skyla hosts to give you the complete Porsche experience.</p>
             </div>
         </div>
     );
